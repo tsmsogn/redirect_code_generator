aadad4348a82af156bdc13dc48c2f909d73c5a7a
tsmsogn <tsmsogn@gmail.com>
2017-11-24 10:20:27 +0900

    Update

--- lib/redirect_code_generator.rb
+++ lib/redirect_code_generator.rb
@@ -1,5 +1,9 @@
+require "redirect_code_generator/generator/apache"
 require "redirect_code_generator/version"
 
 module RedirectCodeGenerator
-  # Your code goes here...
+  def self.create_apache_redirect_code(old, new, permanent = true, escape = true)
+    generator = Generator::Apache.new(old, new, permanent, escape)
+    generator.create_redirect_code
+  end
 end
--- spec/redirect_code_generator_spec.rb
+++ spec/redirect_code_generator_spec.rb
@@ -4,4 +4,18 @@ describe RedirectCodeGenerator do
   it "has a version number" do
     expect(RedirectCodeGenerator::VERSION).not_to be nil
   end
+
+  it ".create_apache_redirect_code" do
+    code = <<CODE
+# 301 /old.html -> /new.html
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteRule ^/old\\\.html$ /new.html? [R=301,L]
+</IfModule>
+CODE
+
+    old = "/old.html"
+    new = "/new.html"
+    expect(RedirectCodeGenerator.create_apache_redirect_code(old, new)).to eq code
+  end
 end
