ac3fc2a926b4cba842385f28860fbfb784fd5ee4
tsmsogn <tsmsogn@gmail.com>
2019-02-27 17:59:12 +0900

    Codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "redirect_code_generator"
